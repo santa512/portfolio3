bb8128175e7add054f139fbcd568ee7c55e2fd26
Santa <157872827+santa512@users.noreply.github.com>
2025-02-07 10:10:09 -0500

    Update Exp

--- src/constants/index.js
+++ src/constants/index.js
@@ -260,32 +260,32 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: 'Framer',
-    pos: 'Lead Web Developer',
-    duration: '2022 - Present',
+    name: 'Odoo',
+    pos: 'Senior Software Engineer',
+    duration: '04/2020 - Present',
     title:
-      'Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.',
-    icon: '/assets/framer.svg',
+      'Odoo is a US-based software company that develops business suite applications for various industries. I work on implementing and customizing its platform to meet specific business needs.',
+    icon: '/assets/Odoo.svg',
     animation: 'victory',
   },
   {
     id: 2,
-    name: 'Figma',
-    pos: 'Web Developer',
-    duration: '2020 - 2022',
+    name: 'MindBridge',
+    pos: 'Software Development Engineer',
+    duration: '11/2018 - 03/2020',
     title:
-      'Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.',
-    icon: '/assets/figma.svg',
+      'At MindBridge AI, I joined early in the startup journey, helping build AI-driven financial tools and shaping products to meet the needs of auditors and finance teams.',
+    icon: '/assets/mindbridge.svg',
     animation: 'clapping',
   },
   {
     id: 3,
-    name: 'Notion',
-    pos: 'Junior Web Developer',
-    duration: '2019 - 2020',
+    name: 'Sylogist',
+    pos: 'Software Developer',
+    duration: '02/2016 - 09/2018',
     title:
-      'Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.',
-    icon: '/assets/notion.svg',
+      'At Sylogist, I worked on developing SaaS applications, including donor and fundraising systems, designed to support the unique needs of non-profit organizations and educational institutions.',
+    icon: '/assets/sylogist.svg',
     animation: 'salute',
   },
 ];
